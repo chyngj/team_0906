b52d7a1f7df016cec479d2db984d52542e5a3943
ehddud2000 <dud1030898@gmail.com>
2024-09-06 13:34:47 +0900

    create_insert_id

--- create.py
+++ create.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-def create_customer(name, gender, email, birthYear):
+def create_customer(id, name, gender, email, birthYear):
     conn = mysql.connector.connect(
         host='192.168.0.92',
         user='test',
@@ -9,8 +9,8 @@ def create_customer(name, gender, email, birthYear):
     )
     cursor = conn.cursor()
 
-    sql = "INSERT INTO customers (name, gender, email, birthYear) VALUES (%s, %s, %s, %d)"
-    values = (name, gender, email, birthYear)
+    sql = "INSERT INTO customers (id, name, gender, email, birthYear) VALUES (%s, %s, %s, %s, %d)"
+    values = (id, name, gender, email, birthYear)
     
     try:
         cursor.execute(sql, values)
@@ -23,5 +23,5 @@ def create_customer(name, gender, email, birthYear):
         conn.close()
 
 # Example usage
-create_customer('John', 'man', 'john.doe@example.com', '2024')
+create_customer('John id', 'John', 'man', 'john.doe@example.com', '2024')
 
